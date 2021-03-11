63681111fce3d759dd5f8623daa333fc0aac018a
Morgan Stanley <morganpstanley@gmail.com>
2021-03-11 14:46:38 -0700

    rename frontend -> docs, add ChessArea component

---
+++ docs/src/components/ChessArea.vue
@@ -0,0 +1,15 @@
+<template>
+  <chessboard />
+</template>
+
+<script>
+import {chessboard} from 'vue-chessboard'
+import 'vue-chessboard/dist/vue-chessboard.css'
+
+export default {
+  name: 'ChessArea',
+  components: {
+    chessboard  
+  }
+}
+</script>
---
+++ docs/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="header">
+    <div id="logo">
+      <img src='../assets/lightning.svg' id="logo-image" alt="lighting-bolt" />
+        <span id="chess">CHESS</span>
+        <span id="battery">BATTERY</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+#header {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 4rem;
+  background-color: seagreen;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#logo-image {
+  filter: drop-shadow(1px 1px 3px yellow);
+  width: 1rem;
+}
+
+#logo{
+  font-size: 1.4rem;
+  color: seashell;
+}
+
+#logo-area {
+  margin: 10px;;
+}
+
+span#battery {
+  font-weight: 700;
+}
+</style>
