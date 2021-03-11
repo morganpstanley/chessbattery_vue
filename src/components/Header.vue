6c39f2693a249d566fb8bf7c55b7388f6f4cbb67
Morgan Stanley <morganpstanley@gmail.com>
2021-03-11 13:03:52 -0700

    .

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="header">
+    <div id="logo">
+      <img src='../assets/images/lightning.svg' id="logo-image" alt="lighting-bolt" />
+        <span id="chess">CHESS</span>
+        <span id="battery">BATTERY</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+#header {
+  width: 100vw;
+  height: 4rem;
+  background-color: seagreen;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#logo-image {
+  filter: drop-shadow(1px 1px 3px yellow);
+  width: 1rem;
+}
+
+#logo{
+  font-size: 1.4rem;
+  color: seashell;
+}
+
+#logo-area {
+  margin: 10px;;
+}
+
+span#battery {
+  font-weight: 700;
+}
+</style>
